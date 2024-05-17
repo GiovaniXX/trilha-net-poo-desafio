0912de5a308842ef860aff140061a0bb789358cf
GiovaniXX <giovani_chaves@hotmail.com>
2024-05-17 18:58:21 -0300

    Implementação do código...

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("123456", "ModeloNokia", "IMEI123", 64);
+        Iphone iphone = new Iphone("7891011", "ModeloIphone", "IMEI456", 128);
+
+        Console.WriteLine($"Nokia - Número: {nokia.Numero}");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Console.WriteLine($"iPhone - Número: {iphone.Numero}");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
